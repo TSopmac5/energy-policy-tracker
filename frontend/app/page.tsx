e894599639510a5cab0ddc263549e2d7b5657a80
Tyler Casillas <cam.ty444@gmail.com>
2026-06-09 18:55:57 -0700

    Initial project setup and homepage

---
+++ frontend/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+  <main className="p-12">
+    <h1 className="text-4xl font-bold">
+      Energy Policy Tracker
+    </h1>
+
+    <p className="mt-4 text-lg">
+      Tracking energy policy across federal,
+      regional, state, county, city, and utility
+      governance.
+    </p>
+  </main>
+);
+    }
